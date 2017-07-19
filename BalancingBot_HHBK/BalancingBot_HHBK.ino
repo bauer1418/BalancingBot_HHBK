a8f1bde5b44ec87d964276a42d2d1f6b6be3a5ce
bauer1418 <bauer1418@gmail.com>
2017-07-19 02:42:09 +0200

    Start

---
+++ BalancingBot_HHBK/BalancingBot_HHBK.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BalancingBot_HHBK.ino
+ Created:	19.07.2017 02:36:37
+ Author:	Andreas Bauer
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	//fgfgdfgdf
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
